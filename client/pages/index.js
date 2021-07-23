fc63bdb18830fac6802d3a1ab5d9a9a7bd97dbaf
noh-yj <goky456@gmail.com>
2021-07-24 00:20:06 +0900

    upload rest api

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,21 @@
-const Home = () => {
+import MsgList from '../components/MsgList';
+import { fetcher } from '../fetcher';
+
+const Home = ({ smsgs, users }) => {
   return (
     <>
-      <h1>simple sns</h1>
+      <h1>SIMPLE SNS</h1>
+      <MsgList smsgs={smsgs} users={users} />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const smsgs = await fetcher('get', '/messages');
+  const users = await fetcher('get', '/users');
+  return {
+    props: { smsgs, users },
+  };
 };
 
 export default Home;
